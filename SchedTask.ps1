dd1b2da4b6d47fbabc4d4c9c6019a8ae424b0290
tricksta123 <trevor@virtulus.co.uk>
2017-08-29 09:37:11 +0100

    Update SchedTask.ps1

--- SchedTask.ps1
+++ SchedTask.ps1
@@ -4,6 +4,4 @@ $Trigger = New-ScheduledTaskTrigger -At "02:00" -Daily -RandomDelay (New-TimeSpa
 
 $Settings = New-ScheduledTaskSettingsSet
 
-$SchedTask = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -Description "This script will clear down Event Logs and DSC log files on a host and will be launched as a daily Scheduled Task"
-
-Register-ScheduledTask -Action $action -TaskName "Daily Log Cleanup" -Description "This script will clear down Event Logs and DSC log files on a host and will be launched as a daily Scheduled Task" -RunLevel Highest -Settings $settings -TaskPath Housekeeping -Trigger $trigger -User System
+Register-ScheduledTask -Action $action -TaskName "Daily Log Cleanup" -Description "This script will clear down Event Logs and DSC log files on a host and will be launched as a daily Scheduled Task" -RunLevel Highest -Settings $settings -TaskPath Microsoft\Windows\Housekeeping -Trigger $trigger -User System
